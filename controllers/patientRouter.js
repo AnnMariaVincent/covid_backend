ef3660d1a4038684b065b16431f3dceb69d456e8
AnnMariaVincet <annmariavincent05@gmail.com>
2024-02-08 18:16:19 +0530

    search patient

--- controllers/patientRouter.js
+++ controllers/patientRouter.js
@@ -17,5 +17,12 @@ router.post("/add",async(req,res)=>{
 res.json(data)
     })
 
+    router.post("/search",async(req,res)=>{
+        let input=req.body
+        let data=await patientModel.find(input)
+       res.json(data)
+     })
+
+
 
 module.exports=router
